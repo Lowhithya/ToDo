b746dc0c63571c95576fbe7a48600adb480d7a5a
Lowhithya <lowhithyakona@gmail.com>
2024-08-26 07:23:47 +0530

    Initial commit

--- toDoApp/views.py
+++ toDoApp/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+from django.shortcuts import render, redirect # type: ignore
+from django.contrib.auth.models import User # type: ignore
+from django.contrib.auth import authenticate, login, logout # type: ignore
+from django.contrib import messages # type: ignore
 from .models import todo
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required # type: ignore
 # Create your views here.
 @login_required
 def home(request):
